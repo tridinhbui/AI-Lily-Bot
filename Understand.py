95330ca4d2265b0cf48c362784cbd24c73d840cf
Tri D Bui <51746347+tridinhbui@users.noreply.github.com>
2022-10-03 10:28:37 -0500

    Add files via upload

---
+++ Understand.py
@@ -0,0 +1,14 @@
+you = "Hello my dear"
+
+if you ==  "":
+    robot_brain = "I can't hear you"
+elif you == "hello":
+    robot_brain = "Hello Tri Bui"
+elif you == "can you tell me a story":
+    robot_brain = "No bro, I hate you haha"
+else:
+    robot_brain = "You shut up"
+
+print(robot_brain)
+
+
